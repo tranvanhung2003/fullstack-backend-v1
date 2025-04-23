bc37033ebee1a6f271c4cd182025f7512f9ff200
tranvanhung2003 <tranvanhan29042007@gmail.com>
2025-04-23 16:22:11 +0700

    hello world voi express

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-const { createServer } = require("node:http"); // node.js
+const express = require("express");
+// import express from "express";
+const app = express();
+const port = 8081;
 
-const hostname = "localhost"; // localhost
-const port = 3000;
-
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World\nTran Van Hung");
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
