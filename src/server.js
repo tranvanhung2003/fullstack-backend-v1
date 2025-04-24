507906f661f2748bebef8649cb41a551c515f423
tranvanhung2003 <tranvanhan29042007@gmail.com>
2025-04-24 17:58:16 +0700

    static files

--- src/server.js
+++ src/server.js
@@ -10,6 +10,9 @@ const hostname = process.env.HOST_NAME;
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+// config static files
+app.use(express.static(path.join(__dirname, "public")));
+
 // khai báo route
 app.get("/", (req, res) => {
   res.send("Hello World!");
