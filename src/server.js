ef06f4d471c93b387180daaf422d26a057a8a116
tranvanhung2003 <tranvanhan29042007@gmail.com>
2025-06-09 10:27:43 +0700

    update

--- src/server.js
+++ src/server.js
@@ -8,6 +8,18 @@ const app = express(); // app express
 const port = process.env.PORT || 8888; // port => hardcode .uat .prod
 const hostname = process.env.HOST_NAME;
 
+// Kiểm tra kết nối MySQL
+connection.getConnection((err, conn) => {
+  if (err) {
+    console.error("Lỗi kết nối đến MySQL database:", err);
+
+    return;
+  }
+
+  console.log("Kết nối MySQL thành công!");
+  conn.release(); // Trả kết nối về pool
+});
+
 configViewEngine(app);
 
 // khai báo route
